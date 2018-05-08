fdf587c2e2028a4dd075baea406b8807440ea52d
agusedyc <edyagusc@gmail.com>
2018-05-08 22:20:57 +0700

    check integras

--- siteharvest/ceksite.sh
+++ siteharvest/ceksite.sh
@@ -8,22 +8,9 @@ IFS="|"
 while read site
 do
         echo "Site ===============================================================================================================>>> $site"
-        ping -c2 $site
-        #Create User
-        #curl -d '{"name":"'$username'", "email":"'$email'","password":"'$password'","fullname":"'$organization'"}' -X POST $site/api/3/action/user_create
-        #Create Organization
-        #curl -H "Authorization:$apikey" -d '{"name":"'$username'","description":"'$organization' '$kabkota'","title":"'$organization'"}' -X POST $site/api/3/action/or$
-        #Create User Organization
-        #curl -H "Authorization:$apikey" -d '{"id":"'$username'","username":"'$username'","role":"admin"}' -X POST $site/api/3/action/organization_member_create
-        #Tampilan Data
-        #echo "Site : $site"
-        #echo "User : $username"
-        #echo "Pass : $password"
-        #echo "Org  : $organization"
-        #echo "Email: $email"
-        #echo "Auth : $apikey"
-        #echo "Desc : $organization $kabkota"
-        echo ""
+        #ping -c2 $site
+	su -c "http --ignore-stdin --headers $site"
+	echo ""
 done < $INPUT
 IFS=$OLDIFS
 
